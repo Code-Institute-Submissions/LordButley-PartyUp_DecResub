5629554df9b49678e2bbe6f97654311fe7774e1f
LordButley <joshjwwilson@gmail.com>
2022-07-15 09:17:14 +0000

    create index.html and create PostList class in views.py

--- whiteboard/views.py
+++ whiteboard/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-
 # Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-most_recent')
+    template_name = 'index.html'
+    
