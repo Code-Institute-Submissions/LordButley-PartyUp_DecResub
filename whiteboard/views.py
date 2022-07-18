ca309cc269493fd708f35e11116c94fa52018105
LordButley <joshjwwilson@gmail.com>
2022-07-18 17:02:23 +0000

    add rocket_league and dead_by_daylight html files and wire up

--- whiteboard/views.py
+++ whiteboard/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 # Create your views here.
@@ -9,3 +9,14 @@ class PostList(generic.ListView):
     queryset = Post.objects.order_by('-most_recent')
     template_name = 'index.html'
     
+class DeadByDaylight(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-most_recent')
+    template_name = 'dead_by_daylight.html'
+    # paginate_by = 10
+
+class RocketLeague(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-most_recent')
+    template_name = 'rocket_league.html'
+    # paginate_by = 10
