4d5c43b1688f055df84cd65458def5a25877e079
LordButley <joshjwwilson@gmail.com>
2022-07-25 10:57:16 +0000

    debug false

--- partyup/settings.py
+++ partyup/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
 ALLOWED_HOSTS = ['partyupgaming.herokuapp.com', 'localhost']
 
 
